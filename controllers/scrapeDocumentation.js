2d44a4241f78ff9725823cb46be4a55b09edbec2
kaif-0 <kaif8700979251@gmail.com>
2025-06-27 14:12:48 +0530

    timeout error handled

--- controllers/scrapeDocumentation.js
+++ controllers/scrapeDocumentation.js
@@ -66,7 +66,7 @@ const worker = new Worker(
             timeout: 5 * 60 * 1000,
           })
           .catch((err) => {
-            console.log(err);
+            console.log("Goto Error: ", err);
             errorOccurred = true;
           });
         if (errorOccurred) continue;
